3c4c05c224e96f44244227526f7892e943f522a8
Amos Jones <azjones227@gmail.com>
2015-11-16 14:25:04 -0500

    pig v2 all functions for pig.js written

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
-# JS-PIG
+# JS-PIG by Amos Jones
+
 JavaScript version of dice game, PIG
+
+PIG is an old timey dice game that's alot of fun. So we're going to code it.
+
+The Rules
+
+A player rolls two dice. The sum gives them a score for that turn. They can bank the turn score into their total score and end their turn, or they can roll again to try to add to their turn score.
+
+If either of the two dice is a 1, they are a Pig. Their score for that turn is reset to 0 their turn ends.
+
+If both dice are a 1, aka Snake-eyes, aka DOUBLE PIG, their TOTAL score is reset to 0 and their turn ends.
+
+If they roll doubles of any other kind, they are compelled to roll again and cannot bank.
+
+Pseudocode First
+
+Talk about this in your pair and share ideas before you write any code. Write down what classes you will need, and what methods and properties they will need to have.
+
+Keep it object oriented. Otherwise you will end up with a Big Ball of Mud
+
+Use Python's random library for the dice.
+
+First Iteration
+
+Two players should be able to play up to a score of their choice. It would be nice if they could choose their winning score setting using command line arguments / ARGV.
+
+Second Iteration
+
+Program an AI to play PIG against. You decide how its play style. Using command line arguments, players should be able to decide if they want to play 2 player or solo. Maybe players can also choose a difficulty setting that determines how tight it's play style is.
+
+Acknowledgements:
+Rosina Jones for her unwavering support and belief in me and my potential.  Omar A.H. Jones and Amos Z. Jones Jr. for being the motivations behind everything I do.  Byte Academy for their tutelage and support.  William Mantly for tireless mentoring, consulting, during immersion hours and during his personal time.  Thomas Harvey for advice on different techniques related to game state tracking. Kai Chuang for introducing me to the liquid fuel that keeps me going in all projects.
--- pig.js
+++ pig.js
@@ -1,49 +1,106 @@
-var di = function(sides) {
+var Di = function(sides) {
 	this.sides = sides;
+	this.current_value = 0;
 }
 
-di.prototype.roll = function() {
-	return Math.floor(Math.random() * (this.sides - 1)) + 1;
+Di.prototype.roll = function() {
+	this.current_value = Math.floor(Math.random() * (this.sides - 1)) + 1;
+	return this.current_value;
 }
 
-var player = function(name) {
+var Player = function(name) {
 	this.username = name;
 	this.bankscore = 0;
 }
 
-player.prototype.addScore = function(turn_score) {
+Player.prototype.addScore = function(turn_score) {
 	this.bankscore = this.bankscore + turn_score;
 	return this.bankscore;	
 }
 
-player.prototype.pig = function() {
+Player.prototype.pig = function() {
 	this.bankscore = 0;
 	return this.bankscore;
 }
 
-var game = function() {
+var Game = function() {
 	this.turn_limit;
 	this.win_score;
 	this.turn_score = 0;
 	this.turn_count = 0;
 	this.player_list = [];
 	this.dice = [];
+	this.current_leader;
+	this.current_player;
 }
 
-/*game.prototype.outcomes = function() {
-	if (this.dice[0].roll === 1 and this.dice[1].roll === 1){
-		current_player
+Game.prototype.outcomes = function() {
+	if ((this.dice[0].current_value === 1) && (this.dice[1].current_value === 1)) {
+		console.log('double pig')
+		this.player_list[this.turn_count].pig();
+		this.end_turn(); 
+	} else if ((this.dice[0].current_value === 1) || (this.dice[1].current_value === 1)) {
+		console.log('pig')
+		this.turn_score = 0;
+		this.end_turn(); 
+	} else if (this.dice[0].current_value === this.dice[1].current_value) {
+		console.log('doubles')
+		this.turn_score += (this.dice[0].current_value + this.dice[1].current_value);
+		this.roll();
+	} else {
+		console.log('turn score increased')
+		this.turn_score += (this.dice[0].current_value + this.dice[1].current_value);
 	}
-}*/
 
-game.prototype.start = function(no_of_players, player_names, win_score, turn_limit) {
+}
+
+Game.prototype.start = function(no_of_players, player_names, win_score, turn_limit) {
 	this.turn_limit = turn_limit;
 	this.win_score = win_score;
 	for (i = 0; i < no_of_players; i++){
-		this.player_list.push( new player(player_names[i]));
+		this.player_list.push( new Player(player_names[i]));
 	}
 	for (i = 0; i < 2; i++){
-		this.dice.push(new di(6));
+		this.dice.push(new Di(6));
 	}
 	return this;
+}
+
+Game.prototype.roll = function() {
+	for (i = 0; i < this.dice.length; i++) {
+		this.dice[i].roll();
+	}
+	return this.dice;
+}
+
+Game.prototype.end_turn = function() {
+	if (this.turn_score > 0) {
+		current_player.addScore(this.turn_score);
+		this.turn_score = 0;
+	}
+	if (current_player.bankscore >= this.win_score) {
+		this.declare_winner(current_player);
+		return;
+	}
+	if (current_player.bankscore > current_leader.bankscore){
+		current_leader = current_player;
+	}
+	this.is_game_over();
+	return;
+}
+
+
+Game.prototype.is_game_over = function() {
+	if (this.turn_limit === 0){
+		this.declare_winner(current_leader);
+		return true;
+	}
+	if (this.player_list.length < 2){
+		this.declare_winner(this.player_list[0]);
+		return true;
+	}
+	this.turn_count = (this.turn_count + 1) % this.player_list.length;
+	this.turn_limit -= 1;
+	this.current_player = player_list[this.turn_count]
+	return false;
 }
