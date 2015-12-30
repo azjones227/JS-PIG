e41592d359a69a55b680b92f964dc18fc915f604
Amos Jones <azjones227@gmail.com>
2015-12-29 20:07:13 -0500

    to_DOM first div works

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
-var myGame;
-
 $(document).ready(function(){
-	$('#new').on('click', '.add', function(event){
-		event.preventDefault();
-    	$("input").after('<label>Player</label> <input class="player" type="text" />');
+    
+    var setup_to_DOM = function() {
+        var game_template = $('#gameTemplate').html();
+        $('#game').html(Mustache.render(game_template, {winScore: pig.win_score, remaining: pig.turn_limit, turnScore: pig.turn_score}))
+        
+    };
+
+    var currentPlayerCount = 2;
+    $('#addPlayer').on('click', function(event){
+        var template = $('#addPlayerTemplate').html();
+        $(this).before(Mustache.render(template, {n: ++currentPlayerCount}))
     });
-    $('#new').on('click', 'submit', function(event) {
-    	event.preventDefault():
+    
+    $('form').on('submit', function(event){
+        event.preventDefault();
+        var players = [];
+        $('input.players').each(function(index, value){
+            players.push($(value).val());
+        });
+        var win_score = $("[name='winScore']").val();
+        var turn_limit = $("[name='turnCount']").val();
+        pig = new Game(players, win_score, turn_limit)
+        $('form').hide();
+        setup_to_DOM();
     });
+
 });
